bd95ca717560566f939384d77326fdc8944080a3
krausetrey <krausetrey@gmail.com>
2023-02-21 20:47:51 -0500

    tidying up a bit

--- main.go
+++ main.go
@@ -7,21 +7,38 @@ import (
 	"gorm.io/gorm"
 )
 
-func main() {
-	fmt.Println("Main entered")
+type User struct {
+	//email with max length of 255, set as PRIMARY KEY, must be UNIQUE
+	Email string `gorm:"primary_key; size:255; unique; not null"`
+	//name with max length of 255
+	Name string `gorm:"size:255; not null"`
+	//user level as an int
+	userLevel int `gorm:"not null"`
+	//password with max length of 25
+	Password string `gorm:"not null; size:25"`
+}
+
+var (
+	db  *gorm.DB
+	err error
+)
+
+func connectAndMigrateDB() {
 	//establish connection to db
-	db, err := gorm.Open(sqlite.Open("usersDB"), &gorm.Config{})
+	db, err = gorm.Open(sqlite.Open("usersDB"), &gorm.Config{})
 	if err != nil {
-		fmt.Println("Error in opening DB")
-		return
+		panic("Error in opening DB")
 	}
 
 	//format db to replicate user struct
 	err = db.AutoMigrate(&User{})
 	if err != nil {
-		fmt.Println("Error in migrating db")
-		return
+		panic("Error in migrating db")
 	}
+}
+
+func main() {
+	fmt.Println("Main entered")
 	fmt.Println("Set up Complete")
 	return
 }
